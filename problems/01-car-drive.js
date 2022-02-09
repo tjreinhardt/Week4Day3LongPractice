d143e183107822633549a9c411d2c8c07e050ce6
Tim Reinhardt <timreinhardtmusic@gmail.com>
2022-02-09 14:30:48 -0800

    We suck

--- problems/01-car-drive.js
+++ problems/01-car-drive.js
@@ -1,5 +1,28 @@
 // Your code here
 
+class Car {
+	constructor(speed) {
+		this.speed = speed;
+	}
+	drive(newSpeed) {
+		this.speed = newSpeed;
+		return this.speed;
+	}
+}
+
+let car = new Car();
+car.drive(0);     // => returns 0
+console.log(car); // => { speed: 0 }
+
+car.drive(10);    // => returns 10
+console.log(car); // => { speed: 10, }
+
+car.drive(50);    // => returns 50
+console.log(car); // -> { speed: 50 }
+
+car.drive(100);   // => returns 100
+console.log(car); // -> { speed: 100 }
+
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
--- problems/02-calculator.js
+++ problems/02-calculator.js
@@ -1,5 +1,39 @@
 // Your code here
 
+class Calculator {
+	constructor() {
+		this.total = 0;
+	}
+
+	add(num) {
+		return this.total += num;
+
+	}
+	subtract(num) {
+
+		return this.total -= num;
+
+	}
+	multiply(num) {
+
+		return this.total *= num;
+
+	}
+	divide(num) {
+
+		return this.total /= num;
+
+	}
+
+
+}
+
+let calculator = new Calculator();
+console.log(calculator.add(50));      // => 50
+console.log(calculator.subtract(35)); // => 15
+console.log(calculator.multiply(10)); // => 150
+console.log(calculator.divide(5));    // => 30
+console.log(calculator.total)         // => 30
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
