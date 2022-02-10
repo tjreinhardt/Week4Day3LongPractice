3b2ff85ec586075b89344be4f9001a148695f022
anti-lag <97247928+anti-lag@users.noreply.github.com>
2022-02-09 21:06:44 -0500

    finished

--- problems/08-call-me-later.js
+++ problems/08-call-me-later.js
@@ -1,11 +1,26 @@
 // Your code here
-
+class CallCenter {
+  constructor(name) {
+    this.name = name;
+  }
+  sayHello = () => {
+    console.log(`Hello this is ${this.name}`);
+  };
+  callMeLater(delay) {
+    setTimeout(() => {
+      return this.sayHello();
+    }, delay);
+  }
+}
+let judy = new CallCenter("Judy");
+judy.sayHello(); // prints "Hello this is Judy"
+judy.callMeLater(1000); // waits one second then prints "Hello this is Judy"
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
 try {
-	module.exports = CallCenter;
+  module.exports = CallCenter;
 } catch {
-	module.exports = null;
+  module.exports = null;
 }
